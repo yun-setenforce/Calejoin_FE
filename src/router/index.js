ade4a588f9e9c3942033abf6e87895532d6500a0
yun-setenforce <extendsabstractclass@gmail.com>
2023-11-10 18:41:41 +0900

    수정

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,5 @@
 // Composables
-<<<<<<< HEAD
 import { createRouter, createWebHistory } from "vue-router";
-
-const routes = [
-  {
-    path: "/",
-    component: () => import("@/layouts/default/Default.vue"),
-=======
-import { createRouter, createWebHistory } from 'vue-router'
-// import JoinShowTimes from "../components/JoinShowTimes.vue";
-
 
 const routes = [
   {
@@ -20,26 +10,22 @@ const routes = [
   {
     path: '/',
     component: () => import('@/layouts/default/Default.vue'),
->>>>>>> cb16213d58495f64b182d3fbab873bf0231f4554
     children: [
       {
         path: "",
         name: "Home",
-<<<<<<< HEAD
         // route level code-splitting
         // this generates a separate chunk (about.[hash].js) for this route
         // which is lazy-loaded when the route is visited.
-=======
->>>>>>> cb16213d58495f64b182d3fbab873bf0231f4554
         component: () =>
           import(/* webpackChunkName: "home" */ "@/views/Home.vue"),
       },
       {
-<<<<<<< HEAD
         path: "PubFeed",
         name: "Public_Feed",
         component: () => import("@/views/Public_Feed.vue"),
-=======
+      },
+      {
         path: "showtime",
         name: "JoinShowTimes",
         component: () =>
@@ -59,7 +45,6 @@ const routes = [
       },
       {
         path: 'dm', name: 'DirectMessage', component: () => import(/* webpackChunkName: "home" */ '@/components/DirectMessage.vue')
->>>>>>> cb16213d58495f64b182d3fbab873bf0231f4554
       },
     ],
   },
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -10,18 +10,10 @@ import Profile from "@/components/Profile.vue";
       <v-container class="px-15 mt-n1" fluid>
         <v-row justify-end>
           <v-col cols="12" md="4">
-<<<<<<< HEAD
-            <Profile />
-          </v-col>
-          <v-col cols="12" md="8"
-            >c
-            <EventCalendar />
-=======
             <profile/>
           </v-col>
           <v-col cols="12" md="8">
             <EventCalendar/>
->>>>>>> cb16213d58495f64b182d3fbab873bf0231f4554
           </v-col>
         </v-row>
       </v-container>
@@ -29,7 +21,6 @@ import Profile from "@/components/Profile.vue";
   </v-app>
 </template>
 
-<<<<<<< HEAD
 <script>
 import Sidebar from "@/components/SideBare.vue";
 import EventCalendar from "@/components/EventCalendar.vue";
@@ -44,7 +35,4 @@ export default {
 };
 </script>
 
-<style></style>
-=======
 <style scoped></style>
->>>>>>> cb16213d58495f64b182d3fbab873bf0231f4554
